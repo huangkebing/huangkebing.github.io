fb3152a8b67cc99130e0fd41f6024d7bbbefdfce
huangkebing <1216033824@qq.com>
2023-12-28 20:05:50 +0800

    blogv3

--- deploy.bat
+++ deploy.bat
@@ -1,24 +1,10 @@
-rmdir /s /q 2023
-rmdir /s /q 2024
-rmdir /s /q 2025
-rmdir /s /q archives
 rmdir /s /q assets
-rmdir /s /q categories
-rmdir /s /q css
-rmdir /s /q fancybox
-rmdir /s /q fontawesome
-rmdir /s /q fonts
 rmdir /s /q images
-rmdir /s /q js
-rmdir /s /q lib
-rmdir /s /q page
-rmdir /s /q tags
-rmdir /s /q webfonts
+rmdir /s /q md
 del 404.html
-del search.xml
 del index.html
 
-xcopy ..\new_blog\public\* .\ /s /e /i /c /h /r /y
+xcopy ..\program-inn\.site\* .\ /s /e /i /c /h /r /y
 
 git add .
 set /p message=commit message:
